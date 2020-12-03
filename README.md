54ae954099ca0e0eb101fa084f9ff0d5a45db8b9
Jin Choo <jinchoo315@gmail.com>
2020-12-03 00:09:08 -0600

    This is save pint and application works.  Figure out the for loop.  Added the gif and screen shot to the README.

--- README.md
+++ README.md
@@ -1,34 +1,39 @@
 # team_profile_generator
 
 ## Introduction
-* As a student devloper, the project was to create team profile generator.
+
+- As a student devloper, the project was to create team profile generator.
 
 ## Table of Contents
+
 1.  Description
 2.  Techologies
 3.  Setup
 4.  License
 
 ## Description:
-1.  When you work at any company, it is imporant to have user friendly team profile generator.  
-2.  In this project, Node.js terminal generates a team profile based on input about the team memebers.  Look at the screen shot as an example.  
-3.  To generate the team profile, 
-    *  Copy npm i inquirer from https://www.npmjs.com/package/inquirer in order to install the following npm package dependencies as specified in the package.jason.  
-    *  inquirer that will prompt you for your inputs from the terminal.  
-    *  When you run node app.js, the application uses the inquirer package to prompt you in the command line with a series of questions about the person role. 
-    *  fs.writeFile is used to generate team profile.  
+
+1.  When you work at any company, it is imporant to have user friendly team profile generator.
+2.  In this project, Node.js terminal generates a team profile based on input about the team memebers. Look at the screen shot as an example.
+3.  To generate the team profile,
+    - Copy npm i inquirer from https://www.npmjs.com/package/inquirer in order to install the following npm package dependencies as specified in the package.jason.
+    - inquirer that will prompt you for your inputs from the terminal.
+    - When you run node app.js, the application uses the inquirer package to prompt you in the command line with a series of questions about the person role.
+    - fs.writeFile is used to generate team profile.
 4.  Technologies:
-    *  JavaScript
-    *  Node CLI
-    *  ES6
-    *  VS-Code Editor
-    *  GitHub Repostory
+    - JavaScript
+    - Node CLI
+    - ES6
+    - VS-Code Editor
+    - GitHub Repostory
 5.  Setup:
-    *  Right click the file app.js to "Open in integrated terminal."
-    *  Node terminal
-    *  Right click the app.js to "Open Preview".
 
-6. License:
-    *  MIT
+    - Right click the file app.js to "Open in integrated terminal."
+    - Node terminal
+    - Right click the app.js to "Open Preview".
 
-![](readme_generator.gif)
+6.  License:
+    - MIT
+
+![](./Assets/team_profile_generator.gif)
+![](./Assets/team_profile_generator.png)
--- app.js
+++ app.js
@@ -1,154 +1,51 @@
-const Manager = require("./Develop/lib/Manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Engineer = require("./Develop/lib/Engineer");
 const Intern = require("./Develop/lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./Develop/lib/htmlRenderer");
-const Employee = require("./Develop/lib/Employee");
-
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Success!");
-  });
-}
+const Manager = require("./Develop/lib/Manager");
 
 const employees = [];
 
-function init() {
-  let employee;
-  // let manager;
+function initApp() {
   startHtml();
-  addManager();
   addMember();
-  // finishHtml();
-}
-function addManager() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the manager's name?",
-        name: "manager",
-      },
-      {
-        type: "input",
-        message: "what is your id?",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "What is your email?",
-        name: "email",
-      },
-      {
-        type: "input",
-        message: "What is your office number?",
-        name: "officeNumber",
-      },
-    ])
-    .then(function (answers) {
-      manager = new Manager(
-        answers.name,
-        answers.id,
-        answers.email,
-        answers.officeNumber
-      );
-      employees.push(manager);
-    });
 }
 
 function addMember() {
-  let role;
   inquirer
     .prompt([
       {
-        type: "input",
-        message: "Enter team memeber's name",
+        message: "Enter team member's name",
         name: "name",
       },
       {
         type: "list",
         message: "Select team member's role",
-        choices: ["Engineer", "Intern"],
+        choices: ["Engineer", "Intern", "Manager"],
         name: "role",
       },
       {
-        type: "input",
         message: "Enter team member's id",
         name: "id",
       },
       {
-        type: "input",
         message: "Enter team member's email address",
         name: "email",
       },
     ])
-    .then(function (answers) {
-      // if (answers.role === "Manager") {
-      //   inquirer
-      //     .prompt({
-      //       type: "input",
-      //       message: "What is the office number?",
-      //       name: "officeNumber",
-      //     })
-      //     .then(function (response) {
-      //       employee = new Manager(
-      //         answers.name,
-      //         answers.id,
-      //         answers.email,
-      //         response.officeNumber
-      //       );
-      //       employees.push(employee);
-      //       role = "Manager";
-      //     });
-      if (answers.role === "Engineer") {
-        inquirer
-          .prompt({
-            type: "input",
-            message: "What is the GibHub URL?",
-            name: "gitHubUrl",
-          })
-          .then(function (response) {
-            employee = new Engineer(
-              answers.name,
-              answers.id,
-              answers.email,
-              response.gitHubUrl
-            );
-            employees.push(employee);
-            role = "Engineer";
-          });
+    .then(function ({ name, role, id, email }) {
+      let roleInfo = "";
+      if (role === "Engineer") {
+        roleInfo = "GitHub username";
+      } else if (role === "Intern") {
+        roleInfo = "school name";
       } else {
-        inquirer
-          .prompt({
-            type: "input",
-            message: "What is name of your school?",
-            name: "school",
-          })
-          .then(function (response) {
-            employee = new Intern(
-              answers.name,
-              answers.id,
-              answers.email,
-              response.school
-            );
-            employees.push(employee);
-          });
+        roleInfo = "office phone number";
       }
-
       inquirer
-        .prompt(
+        .prompt([
           {
-            type: "input",
-            message: `Enter team member's roleInfo`,
+            message: `Enter team member's ${roleInfo}`,
             name: "roleInfo",
           },
           {
@@ -156,38 +53,29 @@ function addMember() {
             message: "Would you like to add more team members?",
             choices: ["yes", "no"],
             name: "moreMembers",
-          }
-        )
+          },
+        ])
         .then(function ({ roleInfo, moreMembers }) {
           let newMember;
           if (role === "Engineer") {
-            newMember = new Engineer(
-              employee.name,
-              employee.id,
-              employee.email,
-              roleInfo
-            );
+            newMember = new Engineer(name, id, email, roleInfo);
           } else if (role === "Intern") {
-            newMember = new Intern(
-              employee.name,
-              employee.id,
-              employee.email,
-              roleInfo
-            );
+            newMember = new Intern(name, id, email, roleInfo);
+          } else {
+            newMember = new Manager(name, id, email, roleInfo);
           }
           employees.push(newMember);
-          console.log(employee);
-          console.log(employees);
           addHtml(newMember).then(function () {
-            // if (moreMembers === "yes") {
-            //   addMember();
-            // } else {
-            finishHtml();
-            // }
+            if (moreMembers === "yes") {
+              addMember();
+            } else {
+              finishHtml();
+            }
           });
         });
     });
 }
+
 function startHtml() {
   const html = `<!DOCTYPE html>
     <html lang="en">
@@ -281,56 +169,4 @@ function finishHtml() {
   console.log("end");
 }
 
-init();
-
-// STEP 1:
-// You ask for the following:
-// - ID
-// - Full name
-// - Email
-// - Role
-//   |
-//   `----- STEP 2:
-//         Get the role answer and present the next set of questions
-//         based on the role
-//         IF role === Manager
-//           - Office Number
-//             |
-//             `---- STEP 3a:
-//                   - Create a new Manager object using Manager Class
-//                   - Add this employee to employees[] array
-//         IF role === Engineer
-//           - Github URL
-//             |
-//             `---- STEP 3b:
-//                   - Create a new Engineer object using Engineer Class
-//                   - Add this employee to employees[] array
-//         ELSE  // Meaning, Intern
-//           - School
-//             |
-//             `---- STEP 3c:
-//                   - Create a new Intern object using Intern Class
-//                   - Add this employee to employees[] array
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+initApp();
